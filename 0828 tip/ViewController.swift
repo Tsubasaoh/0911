af382586ad66b1857a31f7dbfc8d329213b13c0b
“Zorro” <ekwzorrowing@gmail.com>
2018-09-11 01:03:17 +0800

    crazy ALB

--- 0828 tip/ViewController.swift
+++ 0828 tip/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var percentTextField: UITextField!
+    @IBOutlet weak var handlingFeeField: UITextField!
+    @IBOutlet weak var tipsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+   
+    @IBAction func calculate(_ sender: Any) {
+        let priceText = priceTextField.text!//價格
+        let percentText = percentTextField.text!//稅額
+        let handlingFee = handlingFeeField.text!//討人厭的手續費
+        let price = Double(priceText)
+        let percent = Double(percentText)
+        let handling = Double(handlingFee)
+        //預防耍笨...要記得輸入數字啊
+        if priceTextField.text == "" || percentTextField.text == "" || handlingFeeField.text == ""{
+            tipsLabel.text="ಠ╭╮ಠ"
+        }
+        else{
+            tipsLabel.text = String(Double(priceTextField.text!)! * Double(percentTextField.text!)! / 100)
+        }
+        if price != nil,percent != nil,handling != nil{
+            let pay = price! * percent!/100 - handling!
+            tipsLabel.text="\(pay)"//若三者皆不為空值即可顯示計算結果
+            view.endEditing(true)//收收收～收鍵盤
+            
+        }
+    }
 }
-
